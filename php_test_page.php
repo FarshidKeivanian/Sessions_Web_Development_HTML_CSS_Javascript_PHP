55a5b5a9dc3c24ef0407ffe47150732cdc9634bc
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-28 07:34:55 +1000

    Add files via upload

---
+++ php_test_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Test</title>
+</head>
+<body>
+    <h1>PHP section below</h1>
+    <?php
+        echo "Hello, this is PHP inside HTML!";
+    ?>
+</body>
+</html>
