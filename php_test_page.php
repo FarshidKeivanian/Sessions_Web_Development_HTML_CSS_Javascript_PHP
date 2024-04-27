33aa5672f8b64b00186f26279960407c8787bf4e
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-28 08:31:21 +1000

    Update php_test_page.php

--- php_test_page.php
+++ php_test_page.php
@@ -6,9 +6,8 @@
 <body>
     <h1>PHP section below</h1>
     <?php
+        print "Welcome to Lecture Week 7!";
         echo "Hello, this is PHP inside HTML!<br>";
-        print "Hello, this is PHP inside HTML!";
-
     ?>
 </body>
 </html>
