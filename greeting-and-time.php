23879ce68c38de891a08a8c55cb29435aeeb8e5f
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-04-28 08:53:41 +1000

    Add files via upload

---
+++ greeting-and-time.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Welcome <?php echo "to Lecture Week 7"; ?></h1>
+    <p>
+    The server time is <strong><?php echo date("H:i:s"); ?></strong>
+    </p>
+</body>
+</html>
