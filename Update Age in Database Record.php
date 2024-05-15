54e53820640159b774462d649bf035edee2fbc69
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-05-15 10:01:34 +1000

    Update Update Age in Database Record.php

--- Update Age in Database Record.php
+++ Update Age in Database Record.php
@@ -1,27 +1,23 @@
 <html>
 <body>
     <?php
-    $servername = "ict502";
+    $servername = "localhost"; // This should be 'localhost' for XAMPP
+    $dbname = "ict502"; // Database name
     $username = "root";
     $password = "";
-
     // Create connection
-    $conn = new mysqli('localhost', $username, $password, $servername);
-
+    $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     echo "Connected successfully";
-
     $sql = "UPDATE test1 SET age=30 WHERE name='Alex'";
-
     if ($conn->query($sql) === TRUE) {
         echo "The row has been updated successfully";
     } else {
         echo "Error updating the row: " . $conn->error;
     }
-
     $conn->close();
     ?>
 </body>
