3a223e28189d802adb295230c7b873042f0cad8d
Dr. Farshid Keivanian <87278222+FarshidKeivanian@users.noreply.github.com>
2024-05-19 15:53:37 +1000

    Update insert_new_user.php

--- insert_new_user.php
+++ insert_new_user.php
@@ -14,6 +14,7 @@ if ($conn->connect_error) {
 
 // Insert new users with plain text passwords
 $sql1 = "INSERT INTO users (username, password, firstname, lastname, age, country) VALUES ('testuser1', 'userpassword1', 'John', 'Doe', 25, 'USA')";
+$sql2 = "INSERT INTO users (username, password, firstname, lastname, age, country) VALUES ('testuser2', 'userpassword2', 'Jane', 'Doe', 28, 'Canada')";
 
 if ($conn->query($sql1) === TRUE && $conn->query($sql2) === TRUE) {
     echo "New records created successfully";
